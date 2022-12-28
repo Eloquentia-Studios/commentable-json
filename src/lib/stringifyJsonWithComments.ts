74d1276ab850b43e00314c1c0022b9e231c6c017
Esaias Cronelius <esaias@cronelius.se>
2022-12-28 17:15:17 +0100

    Export functions.
    Co-authored-by: Felix Bjerhem Aronsson <felix.b.aronsson@gmail.com>

--- src/lib/stringifyJsonWithComments.ts
+++ src/lib/stringifyJsonWithComments.ts
@@ -1,3 +1,10 @@
+/**
+ * Stringify JSON with comments.
+ *
+ * @param json JSON object to stringify.
+ * @param comments Comment object.
+ * @returns Stringified JSON with comments.
+ */
 const stringifyJsonWithComments = (json: any, comments: any) => {
   const jsonStr = JSON.stringify(json, null, 2)
   const input = jsonStr.split('\n')
@@ -5,11 +12,21 @@ const stringifyJsonWithComments = (json: any, comments: any) => {
   return input.join('\n').trim()
 }
 
+/**
+ * Comment a line.
+ *
+ * @param input Lines of the JSON file.
+ * @param comments Comment object.
+ * @param lineNumber Line number to comment.
+ * @param root Whether the line is the root.
+ * @returns Line number after comment.
+ */
 const commentLine = (input: string[], comments: any, lineNumber: number, root = false): number => {
   console.log('commentLine', lineNumber, input[lineNumber], comments)
   const line = input[lineNumber].trim()
   const key = getLineKey(line)
   const comment = root ? comments : getComment(comments, key)
+
   if (line.endsWith('{')) lineNumber = commentObj(input, comment, lineNumber)
   else if (line.endsWith('[')) lineNumber = commentArray(input, comment, lineNumber)
   else if (root) lineNumber = commentObj(input, comment, lineNumber)
@@ -17,6 +34,15 @@ const commentLine = (input: string[], comments: any, lineNumber: number, root = 
   return lineNumber
 }
 
+/**
+ * Comment an object.
+ *
+ * @param input Lines of the JSON file.
+ * @param comments Comment object.
+ * @param lineNumber Line number where the object starts.
+ * @param inArray Whether the object is in an array.
+ * @returns Line number after object.
+ */
 const commentObj = (input: string[], comments: any, lineNumber: number, inArray = false): number => {
   console.log('commentObj', lineNumber, input[lineNumber], comments)
   lineNumber = commentRoot(input, comments, lineNumber)
@@ -35,6 +61,14 @@ const commentObj = (input: string[], comments: any, lineNumber: number, inArray 
   return lineNumber + 1
 }
 
+/**
+ * Comment an array.
+ *
+ * @param input Lines of the JSON file.
+ * @param comments Comment object.
+ * @param lineNumber Line number where the array starts.
+ * @returns Line number after array.
+ */
 const commentArray = (input: string[], comments: any, lineNumber: number): number => {
   console.log('commentArray', lineNumber, input[lineNumber], comments)
   lineNumber = commentRoot(input, comments, lineNumber)
@@ -60,11 +94,27 @@ const commentArray = (input: string[], comments: any, lineNumber: number): numbe
   return lineNumber + 1
 }
 
+/**
+ * Add a comment to a value.
+ *
+ * @param input Lines of the JSON file.
+ * @param comment Comment to add.
+ * @param lineNumber Line number to add comment to.
+ * @returns Line number after comment.
+ */
 const commentValue = (input: string[], comment: string | undefined, lineNumber: number): number => {
   if (comment) input[lineNumber] = `${input[lineNumber]} // ${comment}`
   return lineNumber + 1
 }
 
+/**
+ * Add a comment to the root of an object.
+ *
+ * @param input Lines of the JSON file.
+ * @param comments Comment object.
+ * @param lineNumber Line number to add comment to.
+ * @returns Line number after comment.
+ */
 const commentRoot = (input: string[], comments: any, lineNumber: number): number => {
   if (comments && comments['_']) {
     input[lineNumber] = `\n${getLineIndent(input[lineNumber])}// ${comments['_']}\n${input[lineNumber]}`
@@ -72,14 +122,39 @@ const commentRoot = (input: string[], comments: any, lineNumber: number): number
   return lineNumber + 1
 }
 
+/**
+ * Get value key from a line.
+ *
+ * @param line Line to get key from.
+ * @returns Key or undefined.
+ */
 const getLineKey = (line: string): string | undefined => {
   return /"*.*":/gi.exec(line)?.[0].replace(/"/g, '').replace(':', '')
 }
 
+/**
+ * Count the indent of a line.
+ *
+ * @param line Line to count indent for.
+ * @returns Indent length.
+ */
 const countIndent = (line: string): number => line.match(/^ */)?.[0].length || 0
 
+/**
+ * Get the indent of a line.
+ *
+ * @param line Line to get indent for.
+ * @returns Indent.
+ */
 const getLineIndent = (line: string): string => ' '.repeat(countIndent(line))
 
+/**
+ * Get a comment from an object by key. If anything is undefined, return undefined.
+ *
+ * @param obj Comment object.
+ * @param key Key to get comment for.
+ * @returns Comment or undefined.
+ */
 const getComment = (obj: any, key: any): any => {
   if (key === undefined) return undefined
   if (obj && obj[key]) return obj[key]
